2a923136ecd4ba0dc789524aadd09c71acf3ce09
Tuyen Mathew Duong <82467450+geekstrident@users.noreply.github.com>
2023-08-01 10:04:21 -0700

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e # exit if anything fails
-git pull
-touch /var/www/regexapp-geekstrident_pythonanywhere_com_wsgi.py
-touch /var/www/templateapp-geekstrident_pythonanywhere_com_wsgi.py
