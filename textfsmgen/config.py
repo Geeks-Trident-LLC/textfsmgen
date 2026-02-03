0ecb26879414ca9159b444928a12575a66ac0f48
Tuyen Mathew Duong <tuyen@geekstrident.com>
2026-02-02 21:08:55 -0800

    chore: bump version from 0.3.1a1 to 0.3.1a2

--- textfsmgen/config.py
+++ textfsmgen/config.py
@@ -29,8 +29,9 @@ import yaml
 
 from textfsmgen.deps import genericlib_version
 from textfsmgen.deps import genericlib_file_module as file
+from textfsmgen.deps import genericlib_dedent_and_strip as dedent_and_strip
 
-__version__ = '0.3.1a1'
+__version__ = '0.3.1a2'
 version = __version__   # noqa
 __edition__ = 'Community'
 edition = __edition__
@@ -97,7 +98,40 @@ class Data:
     license_name = f'TextFSM Generator License'
     copyright_text = f'Copyright \xa9 {years}'
 
-    license = file.read("LICENSE")
+    license = dedent_and_strip(
+        """
+        BSD 3-Clause License
+        
+        Copyright (c) 2021-2040, Geeks Trident LLC
+        All rights reserved.
+        
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are met:
+        
+        1. Redistributions of source code must retain the above copyright notice, this
+           list of conditions and the following disclaimer.
+        
+        2. Redistributions in binary form must reproduce the above copyright notice,
+           this list of conditions and the following disclaimer in the documentation
+           and/or other materials provided with the distribution.
+        
+        3. Neither the name of the copyright holder nor the names of its
+           contributors may be used to endorse or promote products derived from
+           this software without specific prior written permission.
+        
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+        AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+        IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+        DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+        FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+        DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+        SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+        CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+        OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+        OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+        """
+    )
 
     @classmethod
     def get_dependency(cls):
