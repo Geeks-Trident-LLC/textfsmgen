1762356eace9898f2342772e718615eda80b369b
Tuyen Mathew Duong <tuyen@geekstrident.com>
2026-03-14 05:49:10 -0700

    refactor(core.template): create TableTemplateBuilder class

--- textfsmgen/__init__.py
+++ textfsmgen/__init__.py
@@ -9,6 +9,7 @@ Top-level module for the `textfsmgen` package.
 from textfsmgen.core.template import LineParser
 from textfsmgen.core.template import TemplateBuilder
 from textfsmgen.core.template import CategoryTemplateBuilder
+from textfsmgen.core.template import TableTemplateBuilder
 from textfsmgen.config import version
 
 __version__ = version
@@ -17,5 +18,6 @@ __all__ = [
     'LineParser',
     'TemplateBuilder',
     'CategoryTemplateBuilder',
+    'TableTemplateBuilder',
     'version',
 ]
--- textfsmgen/core/template.py
+++ textfsmgen/core/template.py
@@ -33,6 +33,7 @@ from textfsmgen.exceptions import TemplateBuilderError
 from textfsmgen.exceptions import TemplateBuilderInvalidFormat
 
 from textfsmgen.engine.category import CategoryLinesTranslator
+from textfsmgen.engine.tabular import TableTranslator
 
 import logging
 logger = logging.getLogger(__file__)
@@ -644,6 +645,98 @@ class CategoryTemplateBuilder:
         """Generate a Python test script for the current template and test data."""
         return self.builder.create_python_test() if self.builder else ""
 
+
+class TableTemplateBuilder:
+    def __init__(
+        self,
+        user_data='',
+        user_data_file='',
+        test_data='',
+        test_data_file='',
+        column_divider='',
+        column_count=0,
+        column_widths=None,
+        headers=None,
+        header_rows=None,
+        custom_header_text='',
+        starting_from=None,
+        ending_at=None,
+        has_header_row=True,
+        author='',
+        email='',
+        company='',
+        description='',
+        test_script_file='',
+        debug=False
+    ):
+        self.translator = TableTranslator(
+            user_data,
+            column_divider=column_divider,
+            column_count=column_count,
+            column_widths=column_widths,
+            headers=headers,
+            header_rows=header_rows,
+            custom_header_text=custom_header_text,
+            has_header_row=has_header_row,
+            starting_from=starting_from,
+            ending_at=ending_at,
+        )
+
+        self.template_builder_args = dict(
+            user_data=user_data,
+            user_data_file=user_data_file,
+            test_data=test_data,
+            test_data_file=test_data_file,
+            author=author,
+            email=email,
+            company=company,
+            description=description,
+            test_script_file=test_script_file,
+            debug=debug
+        )
+
+        self.builder = None
+
+        if self.translator:
+            snippet = self.translator.to_template_snippet()
+            self.template_builder_args.update(user_data=snippet)
+            self.builder = TemplateBuilder(**self.template_builder_args)
+
+    def __bool__(self): return bool(self.translator)
+
+    def __len__(self): return 1 if self.translator else 0
+
+    @property
+    def template(self):
+        return self.builder.template if self.builder else ""
+
+
+    def verify(self, expected_rows_count=None, expected_result=None,
+               tabular=False, debug=False, ignore_space=False):
+        """Verify parsed test data against expected results."""
+
+        if not self.builder:
+            return False
+
+        return self.builder.verify(
+            expected_rows_count=expected_rows_count,
+            expected_result=expected_result,
+            tabular=tabular,
+            debug=debug,
+            ignore_space=ignore_space
+        )
+
+    def create_unittest(self):
+        """Generate a Python unittest script for the current template and test data."""
+        return self.builder.create_unittest() if self.builder else ""
+
+    def create_pytest(self):
+        """Generate a Python pytest script for the current template and test data."""
+        return self.builder.create_pytest() if self.builder else ""
+
+    def create_python_test(self):
+        """Generate a Python test script for the current template and test data."""
+        return self.builder.create_python_test() if self.builder else ""
 
 
 def get_textfsm_template(
