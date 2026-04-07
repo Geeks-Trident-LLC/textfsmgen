5fdf4834998a31074db5e7fa26cf1260c55f69ad
Tuyen Mathew Duong <tuyen@geekstrident.com>
2026-04-07 11:39:35 -0700

    chore: bump version from 0.4.5 to 0.4.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name='textfsmgen',
-    version='0.4.5',
+    version='0.4.6',
     license='BSD-3-Clause',
     license_files=['LICENSE'],
     description='TextFSM Generator simplifies template creation by converting '
