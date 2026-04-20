ee125415414d42e3134e91c63dc8e640a8f3b8ea
Tuyen Mathew Duong <tuyen@geekstrident.com>
2026-04-19 17:27:55 -0700

    chore(ui): adjust help button width for macOS

--- textfsmgen/ui/snippet.py
+++ textfsmgen/ui/snippet.py
@@ -170,7 +170,7 @@ def build_top_controls(parent, app):
 
         name = f"{label.lower()}_btn"
         width = btn_width + 2 if label in ("Translate", "Generate") else btn_width
-        width = 3 if label == "?" else width
+        width = (1 if ui.is_macos else 2) if label == "?" else width
 
         btn = ui.Button(
             parent,
