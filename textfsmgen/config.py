469a786cb7f96aa99c45aa34e88192ae36de2823
Tuyen Mathew Duong <tuyen@geekstrident.com>
2026-04-27 01:06:16 -0700

    docs(ui.tester): implement Quickstart TextFSM Tester Tool docs

--- textfsmgen/config.py
+++ textfsmgen/config.py
@@ -62,9 +62,10 @@ for name in (
     "high-level-overview", "faq", "textfsm-generator-settings-guide",
     "how-to-use-snippet-translator",
     "how-to-use-regex-builder",
+    "how-to-use-textfsm-tester",
     "demo-snippet-translator", "demo-ios-show-clock",
     "demo-listing-files-in-long-format-on-linux",
-    "demo-listing-files-on-powershell", "demo-linux-file-status-information"
+    "demo-listing-files-on-powershell", "demo-linux-file-status-information",
 ):
     urls[name] = f"{wiki_url}/{name}"
 
