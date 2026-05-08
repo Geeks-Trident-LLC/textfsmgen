1a3ef642f4c7b814fcf91cfc7a29452179e90007
Tuyen Mathew Duong <tuyen@geekstrident.com>
2026-05-07 21:42:55 -0700

    build(test-release): integration golden master (#3) - add pytest option --regen-golden

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,20 @@
 import pytest
+import os
 
 def pytest_addoption(parser):
     parser.addoption(
         "--regen-golden",
         action="store_true",
-        help="Regenerate golden data instead of verifying"
+        default=False,
+        help="Regenerate golden files instead of comparing them."
     )
+
+
+def pytest_configure(config):
+    if config.getoption("--regen-golden"):
+        os.environ["GOLDEN_REGEN"] = "1"
+        print("\n[golden] Regenerating golden files...\n")
+
 
 @pytest.fixture
 def regen_golden(request):
